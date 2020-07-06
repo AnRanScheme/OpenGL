d643f111acabc4ed68bb7a286588e46907feb341
Jonathan <ran.an@binance.com>
2020-07-06 18:08:54 +0800

    [CHG] fix zhushi

--- OpenGLSquare/OpenGL/OpenGL/main.cpp
+++ OpenGLSquare/OpenGL/OpenGL/main.cpp
@@ -103,19 +103,9 @@ void RenderScene(void) {
 void setupRC() {
     //设置清屏颜色（背景颜色）
     glClearColor(0.98f, 0.40f, 0.7f, 1);
-    
     //没有着色器，在OpenGL 核心框架中是无法进行任何渲染的。初始化一个渲染管理器。
     //在前面的课程，我们会采用固管线渲染，后面会学着用OpenGL着色语言来写着色器
     shaderManager.InitializeStockShaders();
-
-//    //指定顶点
-//    //在OpenGL中，三角形是一种基本的3D图元绘图原素。
-//    GLfloat vVerts[] = {
-//        -0.5f,0.0f,0.0f,
-//        0.5f,0.0f,0.0f,
-//        0.0f,0.5f,0.0f
-//    };
-    
     //修改为GL_TRIANGLE_FAN ，4个顶点
     triangleBatch.Begin(GL_TRIANGLE_FAN, 4);
     triangleBatch.CopyVertexData3f(vVerts);
@@ -243,67 +233,38 @@ void SpecialKeys(int key, int x, int y) {
     glutPostRedisplay();
 }
 
+
 int main(int argc,char *argv[]) {
     //设置当前工作目录，针对MAC OS X
-       /*
-        `GLTools`函数`glSetWorkingDrectory`用来设置当前工作目录。实际上在Windows中是不必要的，因为工作目录默认就是与程序可执行执行程序相同的目录。但是在Mac OS X中，这个程序将当前工作文件夹改为应用程序捆绑包中的`/Resource`文件夹。`GLUT`的优先设定自动进行了这个中设置，但是这样中方法更加安全。
-        */
-       gltSetWorkingDirectory(argv[0]);
-       
-       
-       //初始化GLUT库,这个函数只是传说命令参数并且初始化glut库
-       glutInit(&argc, argv);
-       
-       /*
-        初始化双缓冲窗口，其中标志GLUT_DOUBLE、GLUT_RGBA、GLUT_DEPTH、GLUT_STENCIL分别指
-        双缓冲窗口、RGBA颜色模式、深度测试、模板缓冲区
-        
-        --GLUT_DOUBLE`：双缓存窗口，是指绘图命令实际上是离屏缓存区执行的，然后迅速转换成窗口视图，这种方式，经常用来生成动画效果；
-        --GLUT_DEPTH`：标志将一个深度缓存区分配为显示的一部分，因此我们能够执行深度测试；
-        --GLUT_STENCIL`：确保我们也会有一个可用的模板缓存区。
-        深度、模板测试后面会细致讲到
-        */
-       glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH|GLUT_STENCIL);
-       
-       //GLUT窗口大小、窗口标题
-       //glutInitWindowSize(600, 800);
-       glutInitWindowSize(500, 500);
-       glutCreateWindow("Triangle");
-       
-       /*
-        GLUT 内部运行一个本地消息循环，拦截适当的消息。然后调用我们不同时间注册的回调函数。我们一共注册2个回调函数：
-        1）为窗口改变大小而设置的一个回调函数
-        2）包含OpenGL 渲染的回调函数
-        */
-       //注册重塑函数
-       glutReshapeFunc(changeSize);
-       //注册显示函数
-       glutDisplayFunc(RenderScene);
-
-       //注册特殊函数
-      glutSpecialFunc(SpecialKeys);
-       
-       
-       
-       /*
-        初始化一个GLEW库,确保OpenGL API对程序完全可用。
-        在试图做任何渲染之前，要检查确定驱动程序的初始化过程中没有任何问题
-        */
-       GLenum status = glewInit();
-       if (GLEW_OK != status) {
-           
-           printf("GLEW Error:%s\n",glewGetErrorString(status));
-           return 1;
-           
-       }
-       
-       //设置我们的渲染环境
-       setupRC();
-       
-       glutMainLoop();
-       
-
-
-       
-       return  0;
+    //GLTools函数glSetWorkingDrectory用来设置当前工作目录。实际上在Windows中是不必要的，因为工作目录默认就是与程序可执行执行程序相同的目录。但是在Mac OS X中，这个程序将当前工作文件夹改为应用程序捆绑包中的/Resource文件夹。GLUT的优先设定自动进行了这个中设置，但是这样中方法更加安全。
+    gltSetWorkingDirectory(argv[0]);
+    //初始化GLUT库,这个函数只是传说命令参数并且初始化glut库
+    glutInit(&argc, argv);
+    /// 初始化双缓冲窗口，其中标志GLUT_DOUBLE、GLUT_RGBA、GLUT_DEPTH、GLUT_STENCIL分别指双缓冲窗口、RGBA颜色模式、深度测试、模板缓冲区
+    /// --GLUT_DOUBLE`：双缓存窗口，是指绘图命令实际上是离屏缓存区执行的，然后迅速转换成窗口视图，这种方式，经常用来生成动画效果；
+    /// --GLUT_DEPTH`：标志将一个深度缓存区分配为显示的一部分，因此我们能够执行深度测试；
+    /// --GLUT_STENCIL`：确保我们也会有一个可用的模板缓存区。
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH|GLUT_STENCIL);
+    //GLUT窗口大小、窗口标题
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Triangle");
+    /// GLUT 内部运行一个本地消息循环，拦截适当的消息。然后调用我们不同时间注册的回调函数。我们一共注册2个回调函数：
+    /// 1）为窗口改变大小而设置的一个回调函数
+    /// 2）包含OpenGL 渲染的回调函数
+    //注册重塑函数
+    glutReshapeFunc(changeSize);
+    //注册显示函数
+    glutDisplayFunc(RenderScene);
+    //注册特殊函数
+    glutSpecialFunc(SpecialKeys);
+    //初始化一个GLEW库,确保OpenGL API对程序完全可用。在试图做任何渲染之前，要检查确定驱动程序的初始化过程中没有任何问题
+    GLenum status = glewInit();
+    if (GLEW_OK != status) {
+        printf("GLEW Error:%s\n",glewGetErrorString(status));
+        return 1;
+    }
+    //设置渲染环境
+    setupRC();
+    glutMainLoop();
+    return  0;
 }
